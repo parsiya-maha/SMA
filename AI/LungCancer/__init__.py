56e2437bc75a9140d5933cee9252029f9d60cd67
parsiya maha <parsiyamaha@gmail.com>
2024-01-16 17:03:34 +0330

    LungCancer __init__.py

---
+++ AI/LungCancer/__init__.py
@@ -0,0 +1,14 @@
+"""
+Most be 'model.json' and 'model.h5' in this cwd.
+"""
+
+from .PredictImage import predict_image
+import os
+
+base_path = r"D:\Parsia Works\python\Project\AI\LungCancer"
+
+model_json = os.path.join(base_path,"model.json")
+model_h5 = os.path.join(base_path,"model.h5")
+
+def LungCancerPredictImage(image_path:str):
+    return predict_image(image_path,model_h5,model_json)
