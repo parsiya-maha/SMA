f1df55307e93251557df057dde7e98526aa4f312
parsiya maha <parsiyamaha@gmail.com>
2024-01-19 13:06:15 +0330

    add ..LoadModel to AI\..\__init__.py

--- AI/KidneyStone/__init__.py
+++ AI/KidneyStone/__init__.py
@@ -3,12 +3,16 @@ Most be 'model.json' and 'model.h5' in this cwd.
 """
 
 from .PredictImage import predict_image
+from .LoadModel import load_model
 import os
 
 base_path = r"D:\Parsia Works\python\Project\AI\KidneyStone"
 
 model_json = os.path.join(base_path,"model.json")
 model_h5 = os.path.join(base_path,"model.h5")
+
+def KidneyStoneLoadModel():
+    return load_model(model_h5,model_json)
 
 def KidneyStonePredictImage(image_path:str):
     return predict_image(image_path,model_h5,model_json)
