8c187f7c688825134d87413d4d3c49a97c1779ba
parsiya maha <parsiyamaha@gmail.com>
2024-01-24 15:24:41 +0330

    edit TestingModel.py 2

--- AI/__init__.py
+++ AI/__init__.py
@@ -2,3 +2,21 @@ from . import BrainTumors
 from . import LungCancer
 from . import KidneyStone
 from . import ToRecognize
+
+
+def ToRecognizeAndPredictImage(image_path:str):
+    rec = ToRecognize.ToRecognizePredictImage(image_path)
+
+    # cancers ->  ['BrainTumor', 'KidneyStone', 'LungCancer']
+
+    if rec == 'BrainTumor' :
+        from .BrainTumors import BrainTumorsPredictImage as predict_func
+
+    elif rec == 'KidneyStone' :
+        from .KidneyStone import KidneyStonePredictImage as predict_func
+
+    elif rec == 'LungCancer' :
+        from .LungCancer import LungCancerPredictImage as predict_func
+
+
+    return predict_func(image_path)
--- TestingModel.py
+++ TestingModel.py
@@ -1,6 +1,8 @@
 from AI.BrainTumors import BrainTumorsPredictImage
 from AI.LungCancer import LungCancerPredictImage
 from AI.KidneyStone import KidneyStonePredictImage
+from AI.ToRecognize import ToRecognizePredictImage
+from AI import ToRecognizeAndPredictImage
 import os
 import time
 
@@ -19,6 +21,16 @@ _time2 = time.time() - start
 start = time.time()
 anser3 = KidneyStonePredictImage(r"D:\Parsia Works\python\Project\TestingData\Tumor- (342).jpg")
 _time3 = time.time() - start
+
+
+start = time.time()
+anser4 = ToRecognizePredictImage(r"D:\Parsia Works\python\Project\TestingData\Lung-Benign_Tissue.jpeg")
+_time4 = time.time() - start
+
+
+start = time.time()
+anser5 = ToRecognizeAndPredictImage(r"D:\Parsia Works\python\Project\TestingData\Lung-Benign_Tissue.jpeg")
+_time5 = time.time() - start
 
 
 os.system("cls" or "clear")
@@ -40,22 +52,47 @@ print(f"Time    : {_time3}")
 print(f"Predict : {anser3}")
 print()
 print(30*"-")
+print()
+print(f"Time    : {_time4}")
+print(f"Predict : {anser4}")
+print()
+print(30*"-")
+print()
+print(f"Time    : {_time5}")
+print(f"Predict : {anser5}")
+print()
+print(30*"-")
+print()
+print(f"All time    : {_time1+_time2+_time3+_time4+_time5}")
+print(30*"-")
 
 # OUTPUT:
 #------------------------------
 #
-#Time    : 0.9687614440917969
+#Time    : 0.9289171695709229
 #Predict : notumor
 #
 #------------------------------
 #
-#Time    : 0.468017578125
+#Time    : 0.44472670555114746
 #Predict : Lung-Benign_Tissue
 #
 #------------------------------
 #
-#Time    : 0.5106830596923828
+#Time    : 0.4697098731994629
 #Predict : Tumor
 #
 #------------------------------
-
+#
+#Time    : 0.4346308708190918
+#Predict : LungCancer
+#
+#------------------------------
+#
+#Time    : 0.979658842086792
+#Predict : Lung-Benign_Tissue
+#
+#------------------------------
+#
+#All time    : 3.257643461227417
+#------------------------------
